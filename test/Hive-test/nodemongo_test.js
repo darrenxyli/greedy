db8b9c6b936dd7cbfab0a9a5eb7a11381b5b26d6
darrenxyli <darren.xyli@gmail.com>
2014-10-11 16:40:34 -0400

    2014 10 11

--- test/Hive-test/nodemongo_test.js
+++ test/Hive-test/nodemongo_test.js
@@ -2,3 +2,4 @@ var NodeMongoObj = require('../../Hive/nodemongo').NodeMongoObj;
 
 var mongodb = new NodeMongoObj('127.0.0.1', 27017, 'test', "fuck");
 var db = mongodb.nodeMongoInsert({name:'xinyangli', value:'xin'});
+var a = 0;
