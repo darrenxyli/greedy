b58e0352d93b264c3073b9b08734213e8328625e
darrenxyli <darren.xyli@gmail.com>
2014-10-27 18:39:02 -0400

    redis api is done

---
+++ scheduler/index.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# Website: http://www.darrenxyli.com
+# Author: darrenxyli<darren.xyli.com>
+# encoding: utf-8
+
+from flask import Flask
+from flask.ext import restful
+from flask.ext.restful import reqparse
+import redis
+import json
+
+with open('../config.json') as f:
+    config = json.load(f)
+
+pool = redis.ConnectionPool(
+    host=config["REDIS_IPADDRESS_TASK"],
+    port=config["REDIS_PORT_TASK"],
+    db=config["REDIS_DBNAME_BROKER"]
+)
+r = redis.Redis(connection_pool=pool)
+
+app = Flask(__name__)
+api = restful.Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('data', type=str)
+parser.add_argument('project', type=str)
+
+class taskPost(restful.Resource):
+    def get(self):
+        return "POST ONLY", 400
+    def post(self):
+        args = parser.parse_args()
+        data = args['data']
+        project = args['project']
+        r.sadd(project, data)
+        return data, 200
+
+class taskGet(restful.Resource):
+    def get(self, project):
+        return r.spop(project), 200
+
+api.add_resource(taskPost, '/hive')
+api.add_resource(taskGet, '/hive/<string:project>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
