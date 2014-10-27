444a5fb5628f5fad79894470997e4d597996d582
darrenxyli <darren.xyli@gmail.com>
2014-10-27 16:46:26 -0400

    update schedule

--- scheduler/index.js
+++ scheduler/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
 var redisClient = require("./noderedis");
+var config = require("../config.json");
 
-client = new redisClient(6379, '199.231.211.82', {});
+client = new redisClient(config.REDIS_PORT_TASK, config.REDIS_IPADDRESS_TASK, {}) ;
 
 app.post('/hive', function (req, res) {
-	saveType = req.query.urlType;
-	saveUrl = req.query.urlContent;
-    client.NodeRedisUpdate(saveType, saveUrl);
+    saveType = req.query.urlType;
+    saveUrl = req.query.urlContent;
+    // client.NodeRedisUpdate(saveType, saveUrl);
     console.log(client.NodeRedisGet(saveType));
-    res.send("");
+    res.status(404);
+    res.send("good");
 });
 
-app.listen(3000);
+app.listen(3000) ;
