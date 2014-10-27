976808d5734cb673ffe9ac1a8cdc0efa42b2de27
darrenxyli <darren.xyli@gmail.com>
2014-10-27 16:27:10 -0400

    remove pyspider

--- crawler/pyspider/result/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-# vim: set et sw=4 ts=4 sts=4 ff=unix fenc=utf8:
-# Author: darrenxyli<darren.xyli@gmail.com>
-#         http://www.darrenxyli.com
-# Created on 2014-10-19 16:10:19
-
-from result_worker import ResultWorker
--- crawler/pyspider/test/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-# vim: set et sw=4 ts=4 sts=4 ff=unix fenc=utf8:
-# Author: darrenxyli<darren.xyli@gmail.com>
-#         http://www.darrenxyli.com
-# Created on 2014-02-09 10:53:19
