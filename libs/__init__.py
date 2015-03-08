f0ff71ba5d42ecaffaf1d7514b122af81384caf7
darrenxyli <darren.xyli@gmail.com>
2015-03-07 20:48:41 -0500

    refacotry

--- libs/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author: darrenxyli <www.darrenxyli.com>
-# @Date:   2014-10-29 17:24:19
-# @Last Modified by:   darrenxyli
-# @Last Modified time: 2014-10-29 20:33:43
