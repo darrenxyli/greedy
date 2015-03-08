f147f0d0a96db09bafc076494bef809a2fe259c4
darrenxyli <darren.xyli@gmail.com>
2015-03-07 22:57:44 -0500

    uouo

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fetcher"
+	"github.com/darrenxyli/greedy/fetcher"
+	"github.com/darrenxyli/greedy/utils/config"
 )
 
 func main() {
+	config.New()
 	fetcher.CrawlURL("http://www.4porn.com")
 }
---
+++ utils/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"code.google.com/p/gcfg"
+)
+
+type Config struct {
+	Section Section
+}
+
+type Section struct {
+	Name string
+}
+
+func New() *Config {
+	cfg := &Config{
+		Section: Section{
+			Name: "hel",
+		},
+	}
+	return cfg
+}
+
+func (c *Config) LoadConfig(path string) error {
+	err := gcfg.ReadFileInto(c, path)
+	return err
+}
